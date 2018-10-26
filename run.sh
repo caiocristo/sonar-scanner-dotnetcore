2ae243bd0f197c7f226efd35a7ce2d489a55992e
Cristo Caio, (Caio.Cristo@partner.bmwgroup.com) <Caio.Cristo@partner.bmwgroup.com>
2018-10-26 09:30:21 -0300

    Adding coverage path to run

--- run.sh
+++ run.sh
@@ -7,8 +7,9 @@ PROJECT_NAME="${PROJECT_NAME:-ConsoleApplication1}"
 PROJECT_VERSION="${PROJECT_VERSION:-1.0}"
 SONAR_HOST="${HOST:-http://localhost:9000}"
 SONAR_LOGIN_KEY="${LOGIN_KEY:-admin}"
+SONAR_COVERAGE_PATH="${SONAR_COVERAGE_PATH}"
 
-mono /opt/sonar-scanner-msbuild/SonarScanner.MSBuild.exe begin /d:sonar.host.url=$SONAR_HOST /d:sonar.login=$SONAR_LOGIN_KEY /k:$PROJECT_KEY /n:"$PROJECT_NAME" /v:$PROJECT_VERSION
+mono /opt/sonar-scanner-msbuild/SonarScanner.MSBuild.exe begin /d:sonar.host.url=$SONAR_HOST /d:sonar.login=$SONAR_LOGIN_KEY /d:sonar.cs.opencover.reportsPaths=$SONAR_COVERAGE_PATH /k:$PROJECT_KEY /n:"$PROJECT_NAME" /v:$PROJECT_VERSION
 dotnet restore
 dotnet build
 dotnet test
