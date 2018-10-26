da3499b0bcb5e4b875bf856135a3c6d6e7440ce1
Cristo Caio, (Caio.Cristo@partner.bmwgroup.com) <Caio.Cristo@partner.bmwgroup.com>
2018-10-26 13:31:19 -0300

    Changin run to sh

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ SONAR_LOGIN_KEY="${LOGIN_KEY:-admin}"
 SONAR_COVERAGE_PATH="${SONAR_COVERAGE_PATH}"
 
 mono /opt/sonar-scanner-msbuild/SonarScanner.MSBuild.exe begin /d:sonar.host.url=$SONAR_HOST /d:sonar.login=$SONAR_LOGIN_KEY /d:sonar.cs.opencover.reportsPaths=$SONAR_COVERAGE_PATH /k:$PROJECT_KEY /n:"$PROJECT_NAME" /v:$PROJECT_VERSION
-RUN OMCRecallHandler.Tests/scripts/code-coverage
+sh OMCRecallHandler.Tests/scripts/code-coverage
 mono /opt/sonar-scanner-msbuild/SonarScanner.MSBuild.exe end /d:sonar.login=$SONAR_LOGIN_KEY
