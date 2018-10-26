2cbd15c35667f8982871b4bdba2bebf4baa15fa4
Cristo Caio, (Caio.Cristo@partner.bmwgroup.com) <Caio.Cristo@partner.bmwgroup.com>
2018-10-26 15:07:49 -0300

    Changing test to variable

--- run.sh
+++ run.sh
@@ -8,3 +8,8 @@ PROJECT_VERSION="${PROJECT_VERSION:-1.0}"
 SONAR_HOST="${HOST:-http://localhost:9000}"
 SONAR_LOGIN_KEY="${LOGIN_KEY:-admin}"
 SONAR_COVERAGE_PATH="${SONAR_COVERAGE_PATH}"
+COVERAGE_SCRIPT_PATH="${COVERAGE_SCRIPT_PATH}"
+
+mono /opt/sonar-scanner-msbuild/SonarScanner.MSBuild.exe begin /d:sonar.host.url=$SONAR_HOST /d:sonar.login=$SONAR_LOGIN_KEY /d:sonar.cs.opencover.reportsPaths=$SONAR_COVERAGE_PATH /k:$PROJECT_KEY /n:"$PROJECT_NAME" /v:$PROJECT_VERSION
+sh $COVERAGE_SCRIPT_PATH
+mono /opt/sonar-scanner-msbuild/SonarScanner.MSBuild.exe end /d:sonar.login=$SONAR_LOGIN_KEY
